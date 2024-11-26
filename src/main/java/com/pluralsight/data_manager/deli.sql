23c9857721b787b373365d6bd9c89127a09a59da
Joezari Borlongan <bor.joezari@gmail.com>
2024-11-26 13:11:37 -0800

    populate toppings table

--- src/main/java/com/pluralsight/data_manager/deli.sql
+++ src/main/java/com/pluralsight/data_manager/deli.sql
@@ -7,6 +7,7 @@ USE deli;
 CREATE TABLE `Toppings` (
     `toppingID` INTEGER NOT NULL AUTO_INCREMENT,
     `toppingName` VARCHAR(25) NOT NULL,
+    `toppingType` ENUM('Regular', 'Meat', 'Cheese') NOT NULL,
     CONSTRAINT `PK_Toppings` PRIMARY KEY (`toppingID`)
 );
 
@@ -25,3 +26,41 @@ CREATE TABLE `Sandwich_Toppings` (
     CONSTRAINT `FK_Sandwich` FOREIGN KEY (`sandwichID`) REFERENCES `Sandwiches` (`sandwichID`),
     CONSTRAINT `FK_Topping` FOREIGN KEY (`toppingID`) REFERENCES `Toppings` (`toppingID`)
 );
+
+-- populate regulat topping
+INSERT INTO `Toppings` (`toppingName`, `toppingType`)
+VALUES
+    ('Lettuce', 'Regular'),
+    ('Peppers', 'Regular'),
+    ('Onions', 'Regular'),
+    ('Tomatoes', 'Regular'),
+    ('Jalapenos', 'Regular'),
+    ('Cucumbers', 'Regular'),
+    ('Pickles', 'Regular'),
+    ('Guacamole', 'Regular'),
+    ('Mushrooms', 'Regular'),
+    ('Mayo', 'Regular'),
+    ('Mustard', 'Regular'),
+    ('Ketchup', 'Regular'),
+    ('Ranch', 'Regular'),
+    ('Thousand Islands', 'Regular'),
+    ('Vinaigrette', 'Regular'),
+    ('Au Jus', 'Regular'),
+    ('Sauce', 'Regular');
+
+-- populate premium toppings
+INSERT INTO `Toppings` (`toppingName`, `toppingType`)
+VALUES
+    ('Steak', 'Meat'),
+    ('Ham', 'Meat'),
+    ('Salami', 'Meat'),
+    ('Roast Beef', 'Meat'),
+    ('Chicken', 'Meat'),
+    ('Bacon', 'Meat');
+
+INSERT INTO `Toppings` (`toppingName`, `toppingType`)
+VALUES
+    ('American', 'Cheese'),
+    ('Provolone', 'Cheese'),
+    ('Cheddar', 'Cheese'),
+    ('Swiss', 'Cheese');
